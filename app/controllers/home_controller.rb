5ac17654dd9c7602eedce051dfa7788b3b40d969
EC2 Default User <ec2-user@ip-172-31-1-222.ap-northeast-1.compute.internal>
2021-08-10 10:10:56 +0000

    ツイート投稿機能の作成, ユーザー一覧画面の作成

--- app/controllers/home_controller.rb
+++
@@ -1,9 +0,0 @@
-class HomeController < ApplicationController
-    def index
-        @title = "RaiseTech"
-    end
-    
-    def about
-        @about = 'About'
-    end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 Rails.application.routes.draw do
-  get 'tweets/new'
-  get 'tweets/index'
-  get 'tweets/show'
-  get 'tweets/create'
-  get 'users/index'
-  get 'users/show'
+  root 'tweets#index'
+  
   devise_for :users
+  resources :tweets
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # get '/' => 'home#index'
-  root to: "home#index"
-  
-  get '/about' => 'home#about'
-  
 end
